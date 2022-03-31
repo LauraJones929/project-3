23dbbb1d93597ed0105b811855fa41e5756478e8
LauraJones929 <lauraljones19@gmail.com>
2022-03-31 14:36:11 +0000

    Add Recipe category cards and collapsible menu

--- static/js/script.js
+++ static/js/script.js
@@ -5,11 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const check = function() {
     if (document.getElementById('password').value ==
-      document.getElementById('confirm_password').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = '<i class="fas fa-check"></i>';
+        document.getElementById('confirm_password').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = '<i class="fas fa-check"></i>';
     } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = '<i class="fas fa-times"></i>';
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = '<i class="fas fa-times"></i>';
     }
-  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+});
